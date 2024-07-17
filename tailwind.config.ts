d8d8a93ca252dd7635eccc4f34cd423cbe9183af
Jason Heng <jasonheng0304@gmail.com>
2024-07-17 12:33:50 +0100

    feat: add custom height breakpoint 'tall'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      screens: {
+        'tall' : { 'raw' : '(min-height: 700px)'}
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
