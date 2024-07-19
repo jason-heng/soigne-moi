c014f279542763eff2733f35b1391bf89e4dc5e4
Jason Heng <jasonheng0304@gmail.com>
2024-07-19 14:04:42 +0100

    feat: add getDoctor function and edit getDoctors

--- src/_data/doctors.ts
+++ src/_data/doctors.ts
@@ -2,6 +2,7 @@ import 'server-only'
 
 import prisma from "../_lib/db";
 import { verifySession } from "../_lib/session";
+import { Prisma, PrismaClient } from '@prisma/client';
 
 export async function getDoctors() {
     await verifySession()
@@ -9,11 +10,43 @@ export async function getDoctors() {
     return await prisma.doctor.findMany({
         select: {
             id: true,
+            registrationNumber: true,
             firstName: true,
             lastName: true,
             speciality: true,
-            password : true,
-            registrationNumber : true,
+            worksSunday: true,
+            worksMonday: true,
+            worksTuesday: true,
+            worksWednesday: true,
+            worksThursday: true,
+            worksFriday: true,
+            worksSaturday: true,
+
+        }
+    })
+}
+
+export async function getDoctor(doctorId: number) {
+    await verifySession()
+
+    return await prisma.doctor.findUnique({
+        select: {
+            id: true,
+            registrationNumber: true,
+            firstName: true,
+            lastName: true,
+            speciality: true,
+            worksSunday: true,
+            worksMonday: true,
+            worksTuesday: true,
+            worksWednesday: true,
+            worksThursday: true,
+            worksFriday: true,
+            worksSaturday: true,
+
+        },
+        where: {
+            id: doctorId,
         }
     })
 }
