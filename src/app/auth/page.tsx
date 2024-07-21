7cfd9c922af13ebf7b4d4e4577ddea857bcdf518
Jason Heng <jasonheng0304@gmail.com>
2024-07-22 00:58:19 +0100

    fix: remove useSearchParams from auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,25 +1,25 @@
 "use client"
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/_components/ui/tabs'
-import { useSearchParams } from 'next/navigation'
 import { useState } from 'react'
 import { LoginForm } from './_components/LoginForm'
 import { SignupForm } from './_components/SignupForm'
 import Link from 'next/link'
 
-export default function AuthPage() {
-    const searchParams = useSearchParams()
-    const directTab = searchParams.get('tab')
-
-    const [tab, setTab] = useState(directTab === "signup" ? "signup" : "login")
+export default function AuthPage({
+    searchParams,
+}: {
+    searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+    const [tab, setTab] = useState(searchParams?.tab === "signup" ? "signup" : "login")
 
     return (
         <main className='flex justify-center items-center h-screen'>
             <div className='flex flex-col items-center shadow-2xl rounded-lg p-5'>
                 <Link href="/"><h1 className='text-primary text-xl font-bold mb-5'>SoigneMoi</h1></Link>
                 <h1 className='text-2xl font-bold'>Bienvenue !</h1>
-                <h3 className='text-center text-muted-foreground'>Heureux de vous {tab === "login" ? "revoir" : "accueillir"}<br />parmi nous !</h3>
-                <Tabs value={tab} onValueChange={value => setTab(value)} className='flex flex-col justify-center items-center gap-5 m-5 w-[350px]'>
+                <h3 className='text-center text-muted-foreground'>Heureux de vous voir<br />parmi nous !</h3>
+                <Tabs value={tab} onValueChange={setTab} className='flex flex-col justify-center items-center gap-5 m-5 w-[350px]'>
                     <TabsList className='w-[200px]'>
                         <TabsTrigger value="login">Connexion</TabsTrigger>
                         <TabsTrigger value="signup">Inscription</TabsTrigger>
@@ -30,7 +30,8 @@ export default function AuthPage() {
                     <TabsContent value="signup">
                         <SignupForm />
                     </TabsContent>
-                </Tabs></div>
+                </Tabs>
+            </div>
         </main>
     )
 }
