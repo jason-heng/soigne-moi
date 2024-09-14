2086b2bf920d1981dca1d2f781602978b9e775b7
Jason Heng <jasonheng0304@gmail.com>
2024-09-14 21:37:42 +0100

    test: signup function

--- src/app/auth/auth-actions.test.ts
+++ src/app/auth/auth-actions.test.ts
@@ -1,6 +1,6 @@
 import { EMPTY_FORM_STATE } from "@/_lib/to-form-state"
 import { Prisma } from "@prisma/client"
-import { login } from "./actions"
+import { login, signup } from "./actions"
 
 const mockUsers = [
     {
@@ -16,12 +16,14 @@ const mockUsers = [
 ]
 
 jest.mock("bcrypt", () => ({
-    compare: (password: string, hashedPassword: string) => password === hashedPassword
+    compare: (password: string, hashedPassword: string) => password === hashedPassword,
+    hash: () => null
 }))
 
 jest.mock("../../_lib/db", () => ({
     user: {
-        findUnique: (args: Prisma.UserFindUniqueArgs) => mockUsers.find(user => user.email === args.where.email)
+        findUnique: (args: Prisma.UserFindUniqueArgs) => mockUsers.find(user => user.email === args.where.email),
+        create: () => ({id : 1, firstName: "John"})
     }
 }))
 
@@ -98,5 +100,88 @@ describe("Login", () => {
 
         expect(state.message).toBe("Connecté !")
         expect(state.redirect).toBe("/admin")
+    })
+})
+
+describe("Signup", () => {
+    it("returns invalid firstName", async () => {
+        const mockFormData = new FormData()
+        mockFormData.append('firstName', "") // prénom entré est invalide
+        mockFormData.append('lastName', "john")
+        mockFormData.append('email', "john.doe@gmail.com")
+        mockFormData.append('address', "21 rue paris")
+        mockFormData.append('password', 'test123')
+        mockFormData.append('repeatPassword', 'test123')
+
+        const state = await signup(EMPTY_FORM_STATE, mockFormData)
+
+        expect(state.fieldErrors).toEqual({
+            firstName: ["Prénom invalide !"]
+        })
+    })
+
+    it("returns invalid email", async () => {
+        const mockFormData = new FormData()
+        mockFormData.append('firstName', "hamid")
+        mockFormData.append('lastName', "john")
+        mockFormData.append('email', "john.doe") // email est invalide
+        mockFormData.append('address', "21 rue paris")
+        mockFormData.append('password', 'test123')
+        mockFormData.append('repeatPassword', 'test123')
+
+        const state = await signup(EMPTY_FORM_STATE, mockFormData)
+
+        expect(state.fieldErrors).toEqual({
+            email: ["Email invalide !"]
+        })
+    })
+
+    it("returns invalid password", async () => {
+        // tout est valide sauf le mot de passe
+        const mockFormData = new FormData()
+        mockFormData.append('firstName', "john")
+        mockFormData.append('lastName', "john")
+        mockFormData.append('email', "john.doe@gmail.com")
+        mockFormData.append('address', "21 rue paris")
+        mockFormData.append('password', '')
+        mockFormData.append('repeatPassword', 'test123')
+
+        const state = await signup(EMPTY_FORM_STATE, mockFormData)
+
+        expect(state.fieldErrors).toEqual({
+            password: ["Mot de passe invalide !"]
+        })
+    })
+
+    it("returns passwords don't match", async () => {
+        // tout est valide mais les deux mot de passe ne sont pas identiques
+        const mockFormData = new FormData()
+        mockFormData.append('firstName', "john")
+        mockFormData.append('lastName', "john")
+        mockFormData.append('email', "john.doe@gmail.com")
+        mockFormData.append('address', "21 rue paris")
+        mockFormData.append('password', 'test123')
+        mockFormData.append('repeatPassword', 'test321')
+
+        const state = await signup(EMPTY_FORM_STATE, mockFormData)
+
+        expect(state.fieldErrors).toEqual({
+            repeatPassword: ["Mot de passes differents !"]
+        })
+    })
+
+    it("signs up", async () => {
+        const mockFormData = new FormData()
+        mockFormData.append('firstName', "john")
+        mockFormData.append('lastName', "john")
+        mockFormData.append('email', "john.doe@gmail.com")
+        mockFormData.append('address', "21 rue paris")
+        mockFormData.append('password', 'test123')
+        mockFormData.append('repeatPassword', 'test123')
+
+        const state = await signup(EMPTY_FORM_STATE, mockFormData)
+
+        expect(state.message).toBe("Compte crée !")
+        expect(state.redirect).toBe("/patient")
     })
 })
