01b2e846505cb069531967afbf7aa22848598a76
Jason Heng <jasonheng0304@gmail.com>
2024-09-15 19:47:53 +0100

    make doctors list header sticky

--- src/app/admin/doctors/client-components.tsx
+++ src/app/admin/doctors/client-components.tsx
@@ -74,19 +74,19 @@ export function DoctorsList({ doctors }: { doctors: Awaited<ReturnType<typeof ge
     const visibleDoctors = doctors.filter(doctor => `${doctor.firstName.toLowerCase()} ${doctor.firstName.toLowerCase()}`.includes(search.toLowerCase()) || doctor.speciality.toLowerCase().includes(search.toLowerCase()) || doctor.registrationNumber.toString().includes(search.toLowerCase()))
 
     return (
-        <Card className='flex-1 shadow-2xl px-6 border-gray-300 rounded-xl overflow-y-auto '>
-            <CardHeader className='pb-2 px-1'>
+        <Card className='flex-1 shadow-2xl border-gray-300 rounded-xl overflow-y-auto'>
+            <CardHeader className='sticky top-0 bg-white'>
                 <CardTitle className='text-xl text-primary'>Liste des docteurs</CardTitle>
                 <CardDescription className="relative">
                     <MagnifyingGlassIcon className="absolute top-[50%] translate-y-[-50%] right-2" />
                     <Input placeholder="Rechercher..." className='w-full' onChange={e => setSearch(e.target.value)} value={search} />
                 </CardDescription>
             </CardHeader>
-            <div className='my-4 flex gap-3 flex-col'>
+            <CardContent className='flex flex-col gap-3'>
                 {visibleDoctors.map(doctor => (
                     <DoctorCard key={doctor.id} doctor={doctor} />
                 ))}
-            </div>
+            </CardContent>
         </Card>
     )
 }
